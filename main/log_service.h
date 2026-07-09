c75353e12cc58bbccecd7176c2ccd3a607c70942
ITACHIKA <iamwojiaoTM@outlook.com>
2026-07-08 19:50:12 -0700

    save changes

--- main/log_service.h
+++ main/log_service.h
@@ -1,8 +1,5 @@
 #pragma once
-<<<<<<< HEAD
 #include <stddef.h>
-=======
->>>>>>> 50e8f2d7dbc753499fa4925cb48d3e542429cb79
 #include "esp_err.h"
 
 #define LOG_SERVICE_BASE_PATH "/littlefs"
@@ -13,8 +10,4 @@
 esp_err_t log_service_init(void);
 esp_err_t log_service_write(const char *log);
 esp_err_t log_service_clear(void);
-<<<<<<< HEAD
 esp_err_t log_service_print(int line_count);
-=======
-esp_err_t log_service_print(size_t line_count);
->>>>>>> 50e8f2d7dbc753499fa4925cb48d3e542429cb79
